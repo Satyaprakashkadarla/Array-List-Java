149aabfddbbbf3b8f8b28e8e32b79c3436c2e5d2
SatyaPrakash2003 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2024-04-14 20:14:07 +0530

    Add files via upload

---
+++ Introduction To Bit Magic/Bitwise Operators in Cpp/BitwiseOperators.cpp
@@ -0,0 +1,6 @@
+Bitwise operators in C++ are operators that manipulate individual bits of operands. They are often used in low-level programming, embedded systems, and optimization tasks where you need to work at a very granular level with data. Here's a brief overview of bitwise operators in C++:
+
+1.Bitwise AND (&): Performs a bitwise AND operation on each pair of corresponding bits. The result is 1 if both bits are 1, otherwise 0.
+
+cpp code
+int result = a & b;

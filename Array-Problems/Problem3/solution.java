0bbe95d94245e21628a9ee2a10f784c4def22c65
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2024-06-17 21:52:50 +0530

    Add files via upload

---
+++ Array-Problems/Problem3/solution.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+
+public class ArrayProblems {
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        System.out.println("Kth Largest Element: " + findKthLargest(arr, k));
+    }
+}

db39ce724033dc4dca303b467305e1ea74032c4c
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2024-06-26 21:24:08 +0530

    Add files via upload

---
+++ Array-Problems/Problem4/solution.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ArrayProblems {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 7, 1};
+        Arrays.sort(numbers);
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+    }
+}

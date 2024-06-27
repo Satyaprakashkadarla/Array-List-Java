e547da1e76cb5707e77ad7c7820491963b0d2a19
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2024-06-27 21:53:35 +0530

    Add files via upload

---
+++ Array-Problems/Problem7/subarray.java
@@ -0,0 +1,8 @@
+public int maxSubArray(int[] nums) {
+    int maxCurrent = maxGlobal = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+        maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+        maxGlobal = Math.max(maxGlobal, maxCurrent);
+    }
+    return maxGlobal;
+}

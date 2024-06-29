620edeb31a998572741c34dcaf936a00e4de9455
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2024-06-29 23:05:56 +0530

    Add files via upload

---
+++ Array-Problems/Problem9/solution.java
@@ -0,0 +1,9 @@
+public int findDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int num : nums) {
+        if (!set.add(num)) {
+            return num;
+        }
+    }
+    return -1;
+}

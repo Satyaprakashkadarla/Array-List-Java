356301bb80fde0c5c5ffb9bc8c004bf996bb062f
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2024-07-12 20:14:19 +0530

    Add files via upload

---
+++ Array-Problems/Problem26/solution.java
@@ -0,0 +1,22 @@
+public class ConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxOnes = 0;
+        int currentOnes = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                currentOnes++;
+                maxOnes = Math.max(maxOnes, currentOnes);
+            } else {
+                currentOnes = 0;
+            }
+        }
+
+        return maxOnes;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1, 0, 1, 1};
+        System.out.println("Maximum consecutive ones: " + findMaxConsecutiveOnes(nums));
+    }
+}
